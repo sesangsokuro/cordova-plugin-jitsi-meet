b244e8b25ef2f18ef535a000fa53a0f0d8b8ee1e
홍원기 <sesangsokuro@gmail.com>
2018-11-27 15:25:14 +0900

    Request camera and microphone permissions if you do not have camera permissions

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # cordova-plugin-jitsi-meet
 Cordova plugin for jitsi meet react native sdk
+
+---
+
+## update note
+
+permision helper added ( for android ) / 2018-11-27
+
+## how to use
+
+var room_url = 'https://meet.jit.si/room_name';
+
+```javascript
+plugins.JitsiPlugin.loadURL(room_url, null, function(data) {
+  if(data === "CONFERENCE_WILL_LEAVE"){
+    plugins.JitsiPlugin.destroy( function(data) {
+      // on Destroy
+      console.log("Destroy");
+    }, function (err) {
+      // on Error
+      console.log("Error");
+    });
+  }
+}, function(err) {
+  // on load url was not work
+  console.log("Error : ", err);
+});
+```
+
+---
+
+## how to build for ionic
+
+ionic plugin add https://github.com/sesangsokuro/cordova-plugin-jitsi-meet
+ionic plugin add cordova-plugin-compat
+( for cordova permision helper )
+ionic build andorid
--- src/android/JitsiPlugin.java
+++ src/android/JitsiPlugin.java
@@ -3,11 +3,14 @@ package com.cordova.plugin.jitsi;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.PluginResult;
+import org.apache.cordova.PermissionHelper;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import android.util.Log;
+
 import java.util.Map;
 
 import android.os.Bundle;
@@ -16,131 +19,182 @@ import org.jitsi.meet.sdk.JitsiMeetView;
 import org.jitsi.meet.sdk.JitsiMeetViewListener;
 
 import android.view.View;
+
 import org.apache.cordova.CordovaWebView;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 
-public class JitsiPlugin extends CordovaPlugin{
-    private JitsiMeetView view;
-    private static final String TAG = "cordova-plugin-jitsi";
+public class JitsiPlugin extends CordovaPlugin {
+  private JitsiMeetView view;
+  private static final String TAG = "cordova-plugin-jitsi";
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("loadURL")) {
-            String url = args.getString(0);
-            String key = args.getString(1);
-            this.loadURL(url, key, callbackContext);
-            return true;
-        }else if (action.equals("destroy")) {
-            this.destroy(callbackContext);
-            return true;
+  final static String[] permissions = { Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO };
+  public static final int TAKE_PIC_SEC = 0;
+  public static final int REC_MIC_SEC = 1;
+
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    // CB-10120: The CAMERA permission does not need to be requested unless it is declared
+    // in AndroidManifest.xml. This plugin does not declare it, but others may and so we must
+    // check the package info to determine if the permission is present.
+
+    checkPermission();
+
+    if (action.equals("loadURL")) {
+      String url = args.getString(0);
+      String key = args.getString(1);
+      this.loadURL(url, key, callbackContext);
+      return true;
+    } else if (action.equals("destroy")) {
+      this.destroy(callbackContext);
+      return true;
+    }
+    return false;
+  }
+
+
+  private void checkPermission(){
+    boolean takePicturePermission = PermissionHelper.hasPermission(this, Manifest.permission.CAMERA);
+    boolean micPermission = PermissionHelper.hasPermission(this, Manifest.permission.RECORD_AUDIO);
+
+    // CB-10120: The CAMERA permission does not need to be requested unless it is declared
+    // in AndroidManifest.xml. This plugin does not declare it, but others may and so we must
+    // check the package info to determine if the permission is present.
+
+    Log.e(TAG, "tp : " + takePicturePermission);
+    Log.e(TAG, "mp : " + micPermission);
+
+    if (!takePicturePermission) {
+      takePicturePermission = true;
+
+      try {
+        PackageManager packageManager = this.cordova.getActivity().getPackageManager();
+        String[] permissionsInPackage = packageManager.getPackageInfo(this.cordova.getActivity().getPackageName(), PackageManager.GET_PERMISSIONS).requestedPermissions;
+
+        if (permissionsInPackage != null) {
+          for (String permission : permissionsInPackage) {
+            if (permission.equals(Manifest.permission.CAMERA)) {
+              takePicturePermission = false;
+              break;
+            }
+          }
         }
-        return false;
+        Log.e(TAG, "10 : ");
+      } catch (NameNotFoundException e) {
+        // We are requesting the info for our package, so this should
+        // never be caught
+        Log.e(TAG, e.getMessage());
+      }
     }
 
-
-    private void loadURL(final String url, final String key, final CallbackContext callbackContext){
-        Log.e(TAG, "loadURL called" );
-
-
-        cordova.getActivity().runOnUiThread(new Runnable() {
-            public void run() {
-                view = new JitsiMeetView(cordova.getActivity());
-                setJitsiListener(view, callbackContext);
-                view.setWelcomePageEnabled(false);
-                Bundle config = new Bundle();
-                config.putBoolean("startWithAudioMuted", false);
-                config.putBoolean("startWithVideoMuted", false);
-                Bundle urlObject = new Bundle();
-                urlObject.putBundle("config", config);
-                urlObject.putString("url", url);
-                view.loadURLObject(urlObject);
-                cordova.getActivity().setContentView(view);
-            }
-        });
-
-        
+    if(!takePicturePermission){
+      PermissionHelper.requestPermissions(this, TAKE_PIC_SEC,
+        new String[]{ Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO});
     }
+  }
 
-    private void setJitsiListener(JitsiMeetView view, final CallbackContext callbackContext){
-        
-        view.setListener(new JitsiMeetViewListener() {
-            PluginResult pluginResult;
-            private void on(String name, Map<String, Object> data) {
-                Log.d("ReactNative", JitsiMeetViewListener.class.getSimpleName() + " " + name + " " + data);
-            }
+  private void loadURL(final String url, final String key, final CallbackContext callbackContext) {
+    Log.e(TAG, "loadURL called");
 
-            @Override
-            public void onConferenceFailed(Map<String, Object> data) {
-                on("CONFERENCE_FAILED", data);
-                pluginResult = new PluginResult(PluginResult.Status.OK, new JSONObject(data));
-                pluginResult.setKeepCallback(true);
-                callbackContext.sendPluginResult(pluginResult);
-            }
+    cordova.getActivity().runOnUiThread(new Runnable() {
+      public void run() {
+        view = new JitsiMeetView(cordova.getActivity());
+        setJitsiListener(view, callbackContext);
+        view.setWelcomePageEnabled(false);
+        Bundle config = new Bundle();
+        config.putBoolean("startWithAudioMuted", false);
+        config.putBoolean("startWithVideoMuted", false);
+        Bundle urlObject = new Bundle();
+        urlObject.putBundle("config", config);
+        urlObject.putString("url", url);
+        view.loadURLObject(urlObject);
+        cordova.getActivity().setContentView(view);
+      }
+    });
+  }
 
-            @Override
-            public void onConferenceJoined(Map<String, Object> data) {
-                on("CONFERENCE_JOINED", data);
-                pluginResult = new PluginResult(PluginResult.Status.OK, "CONFERENCE_JOINED");
-                pluginResult.setKeepCallback(true);
-                callbackContext.sendPluginResult(pluginResult);
-            }
+  private void setJitsiListener(JitsiMeetView view, final CallbackContext callbackContext) {
 
-            @Override
-            public void onConferenceLeft(Map<String, Object> data) {
-                on("CONFERENCE_LEFT", data);
-                pluginResult = new PluginResult(PluginResult.Status.OK, "CONFERENCE_LEFT");
-                pluginResult.setKeepCallback(true);
-                callbackContext.sendPluginResult(pluginResult);
-            }
+    view.setListener(new JitsiMeetViewListener() {
+      PluginResult pluginResult;
 
-            @Override
-            public void onConferenceWillJoin(Map<String, Object> data) {
-                on("CONFERENCE_WILL_JOIN", data);
-                pluginResult = new PluginResult(PluginResult.Status.OK, "CONFERENCE_WILL_JOIN");
-                pluginResult.setKeepCallback(true);
-                callbackContext.sendPluginResult(pluginResult);
-            }
+      private void on(String name, Map<String, Object> data) {
+        Log.d("ReactNative", JitsiMeetViewListener.class.getSimpleName() + " " + name + " " + data);
+      }
 
-            @Override
-            public void onConferenceWillLeave(Map<String, Object> data) {
-                on("CONFERENCE_WILL_LEAVE", data);
-                pluginResult = new PluginResult(PluginResult.Status.OK, "CONFERENCE_WILL_LEAVE");
-                pluginResult.setKeepCallback(true);
-                callbackContext.sendPluginResult(pluginResult);
-            }
+      @Override
+      public void onConferenceFailed(Map<String, Object> data) {
+        on("CONFERENCE_FAILED", data);
+        pluginResult = new PluginResult(PluginResult.Status.OK, new JSONObject(data));
+        pluginResult.setKeepCallback(true);
+        callbackContext.sendPluginResult(pluginResult);
+      }
 
-            @Override
-            public void onLoadConfigError(Map<String, Object> data) {
-                on("LOAD_CONFIG_ERROR", data);
-                pluginResult = new PluginResult(PluginResult.Status.OK, "LOAD_CONFIG_ERROR");
-                pluginResult.setKeepCallback(true);
-                callbackContext.sendPluginResult(pluginResult);
-            }
-        });
+      @Override
+      public void onConferenceJoined(Map<String, Object> data) {
+        on("CONFERENCE_JOINED", data);
+        pluginResult = new PluginResult(PluginResult.Status.OK, "CONFERENCE_JOINED");
+        pluginResult.setKeepCallback(true);
+        callbackContext.sendPluginResult(pluginResult);
+      }
+
+      @Override
+      public void onConferenceLeft(Map<String, Object> data) {
+        on("CONFERENCE_LEFT", data);
+        pluginResult = new PluginResult(PluginResult.Status.OK, "CONFERENCE_LEFT");
+        pluginResult.setKeepCallback(true);
+        callbackContext.sendPluginResult(pluginResult);
+      }
+
+      @Override
+      public void onConferenceWillJoin(Map<String, Object> data) {
+        on("CONFERENCE_WILL_JOIN", data);
+        pluginResult = new PluginResult(PluginResult.Status.OK, "CONFERENCE_WILL_JOIN");
+        pluginResult.setKeepCallback(true);
+        callbackContext.sendPluginResult(pluginResult);
+      }
+
+      @Override
+      public void onConferenceWillLeave(Map<String, Object> data) {
+        on("CONFERENCE_WILL_LEAVE", data);
+        pluginResult = new PluginResult(PluginResult.Status.OK, "CONFERENCE_WILL_LEAVE");
+        pluginResult.setKeepCallback(true);
+        callbackContext.sendPluginResult(pluginResult);
+      }
+
+      @Override
+      public void onLoadConfigError(Map<String, Object> data) {
+        on("LOAD_CONFIG_ERROR", data);
+        pluginResult = new PluginResult(PluginResult.Status.OK, "LOAD_CONFIG_ERROR");
+        pluginResult.setKeepCallback(true);
+        callbackContext.sendPluginResult(pluginResult);
+      }
+    });
+  }
+
+  private void destroy(final CallbackContext callbackContext) {
+    cordova.getActivity().runOnUiThread(new Runnable() {
+      public void run() {
+        view.dispose();
+        view = null;
+        JitsiMeetView.onHostDestroy(cordova.getActivity());
+        cordova.getActivity().setContentView(getView());
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, "DESTROYED");
+        pluginResult.setKeepCallback(true);
+        callbackContext.sendPluginResult(pluginResult);
+      }
+    });
+  }
+
+  private View getView() {
+    try {
+      return (View) webView.getClass().getMethod("getView").invoke(webView);
+    } catch (Exception e) {
+      return (View) webView;
     }
+  }
 
-    private void destroy(final CallbackContext callbackContext) {
-        cordova.getActivity().runOnUiThread(new Runnable() {
-            public void run() {
-                view.dispose();
-                view = null;
-                JitsiMeetView.onHostDestroy(cordova.getActivity());
-                cordova.getActivity().setContentView(getView());
-                PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, "DESTROYED");
-                pluginResult.setKeepCallback(true);
-                callbackContext.sendPluginResult(pluginResult);
-            }
-        });
-    }
 
-    private View getView() {
-        try {
-            return (View)webView.getClass().getMethod("getView").invoke(webView);
-        } catch (Exception e) {
-            return (View)webView;
-        }
-    }
-
- 
 }
-
